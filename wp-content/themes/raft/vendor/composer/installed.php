03950ed19246e8da611c3fd5f841fe5bbb6cd19b
nkingra <navleenkingra@hotmail.com>
2024-03-23 14:15:34 -0400

    navleen's site

---
+++ wp-content/themes/raft/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/raft',
+        'pretty_version' => 'v1.1.3',
+        'version' => '1.1.3.0',
+        'reference' => 'ca65b480964ada9ded9d697c38b6a3e8bca48aeb',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/raft' => array(
+            'pretty_version' => 'v1.1.3',
+            'version' => '1.1.3.0',
+            'reference' => 'ca65b480964ada9ded9d697c38b6a3e8bca48aeb',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.3.14',
+            'version' => '3.3.14.0',
+            'reference' => '662952078c57b12e4d3af9bc98ef847ea3500206',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
